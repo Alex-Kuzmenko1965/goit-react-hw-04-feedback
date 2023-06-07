c476dc3e292ca07acbd5291fdf73b00aaf612c37
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-06-08 00:37:19 +0300

    Alex

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Section } from './components/Section/Section';
 import { FeedbackOptions } from './components/FeedbackOptions/FeedbackOptions';
@@ -10,32 +10,37 @@ import { Statistics } from './components/Statistics/Statistics';
 // }
 
 export const App = () => {
-  const [good, setGood] = useState(0);
-  console.log(good);
+  const [good, setGood] = useState(0);  
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
   const options = ['good', 'neutral', 'bad'];
-  console.log(options);
+  // console.log(options);
   const total = good + neutral + bad;
-  console.log(total);
+  // console.log(total);
   const positivePercentage = ((good / total) * 100).toFixed(0);
-  console.log(positivePercentage, '%');
+  // console.log(positivePercentage, '%');
   
+  // useEffect(() => {
+  //   const total = good + neutral + bad;
+  //   console.log("total:", total);
+  //   const positivePercentage = ((good / total) * 100).toFixed(0);
+  //   console.log("positivePercentage:", positivePercentage);
+  // }, [good, neutral, bad]);
+ 
   const onLeaveFeedback = (e) => {
     const option = e.target.name;
     console.log(option);
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
     
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
 
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
 
       default:
@@ -43,31 +48,6 @@ export const App = () => {
     }
   }
   
-  useEffect(() => {
-    // const total = good + neutral + bad;
-    console.log("total:", total);
-    // const positivePercentage = ((good / total) * 100).toFixed(0);
-    console.log("positivePercentage:", positivePercentage);
-  }, [good, neutral, bad]);
-
-  // onLeaveFeedback = (event) => {    
-  //   // console.log(event.target.name);
-  //   const option = event.target.name;
-  //   console.log(option);
-  //   this.setState((prevState) => ({
-  //   [option]: (prevState[option] += 1),        
-  //   }));
-  // };
-  
-  // render() {
-    // const { good, neutral, bad } = this.state;
-    // const options = Object.keys(this.state);
-    // console.log(options);
-    // const total = good + neutral + bad;
-    // console.log(total);
-    // const positivePercentage = ((good / total) * 100).toFixed(0);
-    // console.log(positivePercentage);    
-
   return (
     <>    
     <Section title={"Please leave feedback"} />   
